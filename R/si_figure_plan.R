f364857c9b3d2704160fd42bffbe8017d06e89ba
audhalbritter <me@audhalbritter.com>
2024-06-18 12:00:36 +0200

    start new plant for figures in the appendix

---
+++ R/si_figure_plan.R
@@ -0,0 +1,11 @@
+### Supplementary Figures
+
+si_figure_plan <- list(
+
+  # plot ordination for other available nutrients
+  tar_target(
+    name = nutrient_pca,
+    command = plot_pca(other_available_nutrients)
+  )
+
+)
